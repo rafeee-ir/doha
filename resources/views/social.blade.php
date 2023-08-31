da13206e65be9595860c0c9e9a6b6e641f0355bc
Hadi <rafeee.ir@gmail.com>
2023-08-31 11:50:42 +0330

    social

--- resources/views/social.blade.php
+++ resources/views/social.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <a href="https://instagram.com/qatarexpo_ir" title="@qatarexpo_ir">
+    <a href="https://instagram.com/qatarexpo_ir" target="_blank" title="@qatarexpo_ir">
         <img src="{{url('images/instagram_icon.png')}}" width="32px" alt="Instagram">
     </a>
-    <a href="https://t.me/qatarexpo_ir" title="@qatarexpo_ir">
+    <a href="https://t.me/qatarexpo_ir" target="_blank" title="@qatarexpo_ir">
         <img src="{{url('images/telegram_icon.png')}}" width="32px" alt="Telegram">
     </a>
-    <a href="https://wa.me/989370161675?text=Hi%20from%20Qatarexpo.ir" title="09370161675">
+    <a href="https://wa.me/989370161675?text=Hi%20from%20Qatarexpo.ir" target="_blank" title="09370161675">
         <img src="{{url('images/whatsapp_icon.png')}}" width="32px" alt="Whatsapp">
     </a>
 </div>
